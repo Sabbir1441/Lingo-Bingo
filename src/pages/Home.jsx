4adc28824fff21e7af936f3ff54f60a5ac2190fd
Sabbir1441 <sabbirahmed1441@gmail.com>
2024-11-21 02:34:15 +0600

    banner slider added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,41 @@
-
+import img1 from "../assets/1.jpg";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
+import img4 from "../assets/4.avif";
 
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <div className="carousel w-full h-[400px]">
+                <div id="item1" className="carousel-item w-full">
+                    <img
+                        src={img1}
+                        className="w-full" />
+                </div>
+                <div id="item2" className="carousel-item w-full">
+                    <img
+                        src={img2}
+                        className="w-full" />
+                </div>
+                <div id="item3" className="carousel-item w-full">
+                    <img
+                        src={img3}
+                        className="w-full" />
+                </div>
+                <div id="item4" className="carousel-item w-full">
+                    <img
+                        src={img4}
+                        className="w-full" />
+                </div>
+            </div>
+            <div className="flex w-full justify-center gap-2 py-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
+                <a href="#item4" className="btn btn-xs">4</a>
+            </div>
+
+            
         </div>
     );
 };
